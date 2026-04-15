d3da9d43e277e8abdc5d180efa67f7e0b0034e76
Falk B. Schimweg <git@falk.schimweg.de>
2026-04-15 21:55:07 +0200

    stabilized the PySide6 2

--- sources/shiboken6_generator/generator/shiboken/cppgenerator.cpp
+++ sources/shiboken6_generator/generator/shiboken/cppgenerator.cpp
@@ -2864,7 +2864,7 @@ void CppGenerator::writeTypeCheck(TextStream &s, const AbstractMetaType &argType
 static void checkTypeViability(const AbstractMetaFunctionCPtr &func,
                                const AbstractMetaType &type, int argIdx)
 {
-    if (!TypeDatabase::checkApiVersion(ShibokenGenerator::packageName(), { QVersionNumber(6, 11, 0), QVersionNumber(9999, 9999) }))
+    if (!TypeDatabase::checkApiVersion(TypeDatabase::instance()->defaultPackageName(), { QVersionNumber(6, 11, 0), QVersionNumber(9999, 9999) }))
         return;
     const bool modified = argIdx == 0
         ? func->isTypeModified()
